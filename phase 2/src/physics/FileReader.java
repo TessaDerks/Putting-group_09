f7d7e20961065119b87704c62577324aba54531c
TessaDerks <tessa.derks@hotmail.nl>
2020-05-01 10:57:39 +0200

    commended + clean up code

--- phase 2/src/physics/FileReader.java
+++ phase 2/src/physics/FileReader.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class FileReader {
 
     private static double g;
-    private static double weight; // grams
+    private static double weight; 
     private static double mu;
     private static double speed;
     private static double radius;
@@ -15,20 +15,17 @@ public class FileReader {
     private static double goalY;
     private static String height;
 
-    public static Vector2d[] getVelocity() {
-        return velocity;
-    }
-
     private static Vector2d[] velocity;
     private static Vector2d start;
     private static Vector2d goal;
 
-
+    // read file about shots, create array with shots
     public static void fileShot(String fileName){
+        
         String path = fileName;
-
         Scanner input;
 
+        // add text in file to array by line
         ArrayList<String> data = new ArrayList<String>();
         try {
             File file = new File(path);
@@ -38,10 +35,11 @@ public class FileReader {
                 data.add(input.nextLine());
             }
 
-
         } catch (FileNotFoundException e) {
             System.out.println(e);
         }
+
+        // create array where shots will be added to
         int size = (data.size()/2);
         double angle = 0.0;
         double speedd = 0.0;
@@ -49,13 +47,11 @@ public class FileReader {
         velocity = new Vector2d[size];
         int velocityIndex = 0;
 
+        // fill array with shots from file
         for (int i = 0; i < size; i++){
-
             for (int line = i * 2; line < (2 + i * 2); line++) {
 
                 String[] parts = data.get(line).split(" = ");
-
-
                 String names = parts[0];
                 String values = parts[1];
 
@@ -67,30 +63,29 @@ public class FileReader {
                     angle = Double.parseDouble(values);
                     System.out.println(angle);
                 }
-                //velocity[i] = Tools.velFromAngle(angle,speedd);
             }
             velocity[i] = Tools.velFromAngle(angle,speedd);
-
-            System.out.println(velocity[i].get_x() + " " + velocity[i].get_y());
-            //System.out.println(this.velFromAngle(angle, speedd).getX() + " " + this.velFromAngle(angle, speedd).getY());
         }
-
-
     }
 
     public static Vector2d getShot(int index){
         return velocity[index];
     }
 
+    // returns array with all shots
+    public static Vector2d[] getVelocity() {
+        return velocity;
+    }
 
+    // read file for information about the terrain
     public void fileRead(String path1) {
 
         String path = path1;
-
         Scanner input;
 
         ArrayList<String> data = new ArrayList<String>();
 
+        // add text in file to array by line
         try {
             File file = new File(path);
             input = new Scanner(file);
@@ -115,64 +110,49 @@ public class FileReader {
 
             if (list1.contains("g")) {
                 g = Double.parseDouble(values);
-
-                System.out.println(g);
             }
 
             if (list1.contains("m")) {
                 weight = Double.parseDouble(values);
-
-                System.out.println(weight);
             }
 
             if (list1.contains("mu")) {
                 mu = Double.parseDouble(values);
-
-                System.out.println(mu);
             }
 
             if (list1.contains("vmax")) {
                 speed = Double.parseDouble(values);
-
-                System.out.println(speed);
             }
 
             if (list1.contains("tol")) {
                 radius = Double.parseDouble(values);
-
-                System.out.println(radius);
             }
 
             if (list1.contains("startX")) {
                 startX = Double.parseDouble(values);
-                System.out.println(startX);
             }
             if (list1.contains("startY")) {
                 startY = Double.parseDouble(values);
-                System.out.println(startY);
             }
 
             start = new Vector2d(startX, startY);
 
-            if (list1.contains("goalX")) {    //needs adjusting
+            if (list1.contains("goalX")) {    
                 goalX = Double.parseDouble(values);
                 System.out.println(goalX);
 
             }
-            if (list1.contains("goalY")) {    //needs adjusting
+            if (list1.contains("goalY")) {    
                 goalY = Double.parseDouble(values);
-                System.out.println(goalY);
             }
 
             goal = new Vector2d(goalX, goalY);
 
-            if (list1.contains("height")) {      //needs adjusting
-                //String masterFormula = ShuntingYard.postfix(values);
-                //System.out.println(masterFormula);
+            if (list1.contains("height")) {      
                 height = values;
-                System.out.println(values);
             }
         }
+        // send information to build terrain
         SimulateMain.beginning(g, weight, mu, speed, radius, start, goal, height,2);
     }
 
