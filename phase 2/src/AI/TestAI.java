6147716546745a8cbf01abeebee61232b63e583a
TessaDerks <tessa.derks@hotmail.nl>
2020-05-12 15:46:32 +0200

    file usable for experiments to test AI, might delete it from git later

--- phase 2/src/AI/TestAI.java
+++ phase 2/src/AI/TestAI.java
@@ -1,4 +1,27 @@
 package AI;
 
+import physics.*;
+
 public class TestAI {
+
+    public static void main(String []args){
+        // initialize terrain
+        setStart();
+        int popSize = 25;
+        Genetic.testCase = true;
+        SimulateGenetic.initialize(popSize);
+        Genetic.testCase = false;
+    }
+
+    public static void setStart(){
+        double g = 9.81;
+        double mass = 45;
+        double mu = 0.131;
+        double maxV = 30;
+        double radius = 0.2;
+        Vector2d start = new Vector2d(5.0, 5.0);
+        Vector2d goal = new Vector2d(10.0,10.0);
+        String height = "1";
+        SimulateMain.beginning(g, mass, mu, maxV, radius, start, goal, height);
+    }
 }
