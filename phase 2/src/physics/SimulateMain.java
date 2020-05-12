af898982d560d0c46f80becdee4aa2eef5a2be44
TessaDerks <tessa.derks@hotmail.nl>
2020-05-12 15:43:34 +0200

    beginning method added without intializing terrain for testing AI, might want to delete this method later tho

--- phase 2/src/physics/SimulateMain.java
+++ phase 2/src/physics/SimulateMain.java
@@ -5,7 +5,6 @@ import main.Main;
 
 public class SimulateMain {
 
-    //<editor-fold desc="Global Variables">
     private static Vector2d start;
     private static Vector2d flag;
     private static Function2d function;
@@ -16,7 +15,7 @@ public class SimulateMain {
     private static double mu;
     private static double vmax;
     private static double tol;
-    //</editor-fold>
+
 
     // receives information about new terrain and sends information to the right classes
     public static void beginning(double _g, double _m, double _mu, double _vmax, double _tol, Vector2d _start, Vector2d _goal, String _height, int _version) {
@@ -70,6 +69,35 @@ public class SimulateMain {
         }
     }
 
+    public static void beginning(double _g, double _m, double _mu, double _vmax, double _tol, Vector2d _start, Vector2d _goal, String _height) {
+
+        g = _g;
+        m = _m;
+        mu = _mu;
+        vmax = _vmax;
+        tol = _tol;
+        start = _start;
+        flag = _goal;
+        function = new Function2d(_height);
+
+        // Create PuttingSimulator and set all given settings
+        PuttingCourse course = new PuttingCourse(function, flag, start);
+        course.set_mu(mu);
+        course.set_vMax(vmax);
+        course.set_holeTolerance(tol);
+
+        // create physics engine and set all settings for terrain
+        PhysicsEngine engine = new SIESolver(start);
+        engine.set_step_size(0.01);
+        engine.set_h(function);
+        engine.set_m(m);
+        engine.set_g(g);
+        engine.set_v_max(vmax);
+        simulator = new PuttingSimulator(course, engine);
+
+        simulator.set_ball_position(start);
+
+    }
     public static void start(){
         simulator.last_ball_position = start;
     }
