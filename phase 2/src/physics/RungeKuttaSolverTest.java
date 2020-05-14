56d500b0117dd920045437b3cefb26bb8a6d2bbf
StijnCool <hennissenstijn@gmail.com>
2020-05-14 12:23:26 +0200

    Added JUnit test classes: RungeKuttaSolverTest.java SIESolverTest.java and VerletSolverTest.java

---
+++ phase 2/src/physics/RungeKuttaSolverTest.java
@@ -0,0 +1,20 @@
+package physics;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RungeKuttaSolverTest {
+
+    @Test
+    void actTimestep() {
+    }
+
+    @Test
+    void calcG() {
+    }
+
+    @Test
+    void calcH() {
+    }
+}
