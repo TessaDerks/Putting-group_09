3e4cc225149da62ed382e3d81008d9e4a89719fd
TessaDerks <tessa.derks@hotmail.nl>
2020-05-17 15:33:43 +0200

    if hole in one is not possible, shot after is possible

--- phase 2/src/AI/SimulateGenetic.java
+++ phase 2/src/AI/SimulateGenetic.java
@@ -1,35 +1,12 @@
 package AI;
 
+import main.Main;
 import physics.*;
 
 public class SimulateGenetic {
     private static Vector2d start = new Vector2d(0.0, 0.0);
-    private static Vector2d end = new Vector2d(3.0 , 2.0);
-    private static int popSize = 25;
-    private static Function2d function = new Function2d("1");
-
-    /*
-    public static void main(String[] args) {
-        Genetic gen = new Genetic(start, end, popSize, function);
-        gen.initializePopulation();
-        gen.cocktailShaker();
-        System.out.print(gen.getAngle());
 
 
-
-        //xgen.calculateAngle();
-        if (gen.CalculateAmountShots() > 1) {
-            System.out.println(gen.CalculateAmountShots());
-            System.out.println("Hole in one not possible, try different parameters");
-        }
-        else {
-            //gen.calculateAngle();
-            gen.takeFirstShot();
-            gen.finishGame();
-            System.out.println("Your bot scored after " + gen.getGeneration() + " generations!");
-        }
-    }
-    */
 
     // method to call from main to start AI with graphics
     public static void initialize(int popsize){
@@ -43,6 +20,8 @@ public class SimulateGenetic {
         if (gen.CalculateAmountShots() > 1) {
             System.out.println(gen.CalculateAmountShots());
             System.out.println("Hole in one not possible, try different parameters");
+            Main.openNewWindow = true;
+            SimulateMain.simulator.get_engine().resetPosition(start);
         }
         else {
             //gen.calculateAngle();
