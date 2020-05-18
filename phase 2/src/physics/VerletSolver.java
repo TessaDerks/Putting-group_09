33a0fa70ec37c1f674f4cdfd520b980f072bfa90
StijnCool <hennissenstijn@gmail.com>
2020-05-18 18:01:57 +0200

    verlet not de one

--- phase 2/src/physics/VerletSolver.java
+++ phase 2/src/physics/VerletSolver.java
@@ -12,7 +12,7 @@ public class VerletSolver implements PhysicsEngine{
     private Vector2d p;
     private Vector2d last_p;
     private Vector2d v;
-    private Function2d h = new Function2d("1");
+    private Function2d h;
     private Vector2d G;
     private Vector2d H;
     private Vector2d F;
