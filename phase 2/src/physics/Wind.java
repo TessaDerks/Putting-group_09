6be9fba9e3fb519320c9cdca2b68d5d685a09db1
StijnCool <hennissenstijn@gmail.com>
2020-06-11 11:16:23 +0200

    Added Wind with three constructors for random wind, random bounded wind, and specified wind. Added method that creates semi-realistic chance distribution for random wind speeds.

---
+++ phase 2/src/physics/Wind.java
@@ -0,0 +1,46 @@
+package physics;
+
+public class Wind {
+    private double angle;
+    private double totalForce;
+    private Vector2d force;
+
+    // Random force and direction.
+    public Wind(){
+        angle = Math.random() * 360;
+        double b = 30;// Upper bound at 30 m/s -> https://en.wikipedia.org/wiki/Beaufort_scale
+        totalForce = calcBoundedForce(0,b);
+        force = Tools.velFromAngle(angle,totalForce);
+    }
+
+    // Random force and direction, with bounds.
+    public Wind(double angleLowerBound, double angleUpperBound, double forceLowerBound, double forceUpperBound){
+        angle = Math.random() * (angleUpperBound-angleLowerBound) + angleLowerBound;
+        totalForce = calcBoundedForce(forceLowerBound,forceUpperBound);
+    }
+
+    // Chosen force and direction.
+    public Wind(double _angle, double _totalForce){
+        angle = _angle;
+        totalForce = _totalForce;
+        force = Tools.velFromAngle(angle,totalForce);
+    }
+
+    // Calculate a force roughly following frequency of wind speed occurrence, bounded.
+    private double calcBoundedForce(double lB, double uB){
+        int i = 0;
+        double d = uB-lB;
+        double f = Math.random() * d + lB;
+        while(d > 1){
+            d = d/2;
+            if(i % 2 == 0){
+                f -= Math.random() * d;
+            }
+            else{
+                f += Math.random() * d;
+            }
+            i++;
+        }
+        return f;
+    }
+}
