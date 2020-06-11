7055b3effd5d02813c1b07f81c8a088bba94e8b5
TessaDerks <tessa.derks@hotmail.nl>
2020-06-11 16:22:18 +0200

    sand object

---
+++ phase 2/src/physics/Sand.java
@@ -0,0 +1,29 @@
+package physics;
+
+public class Sand {
+    private Vector2d p;
+    private double r;
+    private double friction;
+
+
+
+    public Sand(Vector2d _p, double _r, double _friction){
+        p = _p;
+        r = _r;
+        friction = _friction;
+
+    }
+
+    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Needs to be changed still.
+    public boolean sandHit(Vector2d bp){
+        return (Math.sqrt(Math.pow(bp.get_x() - p.get_x(),2) + Math.pow(bp.get_y() - p.get_y(),2))) <= r;
+    }
+
+    public Vector2d getP() {
+        return p;
+    }
+
+    public double getFriction() {
+        return friction;
+    }
+}
