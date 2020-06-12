84652143a30cffe331d58c3db752e8c0c9380d74
StijnCool <hennissenstijn@gmail.com>
2020-06-12 10:30:22 +0200

    Redid Sand object, old Sand object stored as Sand_old.java

--- phase 2/src/physics/Sand.java
+++ phase 2/src/physics/Sand.java
@@ -1,29 +1,39 @@
 package physics;
 
 public class Sand {
-    private Vector2d p;
-    private double r;
-    private double friction;
+    private Vector2d topLeft;
+    private Vector2d bottomRight;
+    private double friction = 0.9;
 
-
-
-    public Sand(Vector2d _p, double _r, double _friction){
-        p = _p;
-        r = _r;
+    public Sand(Vector2d _topLeft, Vector2d _bottomRight, double _friction){
+        topLeft = _topLeft;
+        bottomRight = _bottomRight;
         friction = _friction;
-
     }
 
-    // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Needs to be changed still.
-    public boolean sandHit(Vector2d bp){
-        return (Math.sqrt(Math.pow(bp.get_x() - p.get_x(),2) + Math.pow(bp.get_y() - p.get_y(),2))) <= r;
+    public boolean coordInSand(Vector2d c){
+        boolean r = false;
+        if(((c.get_x()>=topLeft.get_x())&&(c.get_x()<=bottomRight.get_x())&& ((c.get_y()<=topLeft.get_y())&&(c.get_y()>=bottomRight.get_y())))){
+            r = true;
+        }
+        return r;
     }
 
-    public Vector2d getP() {
-        return p;
-    }
-
+    //<editor-fold desc="Getters & Setters">
     public double getFriction() {
         return friction;
     }
+
+    public Vector2d getBottomRight() {
+        return bottomRight;
+    }
+
+    public Vector2d getTopLeft() {
+        return topLeft;
+    }
+
+    public void setFriction(double friction) {
+        this.friction = friction;
+    }
+    //</editor-fold>
 }
