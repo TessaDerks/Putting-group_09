07f1f9b6469de950345b575719bb48e16d8fc11d
StijnCool <hennissenstijn@gmail.com>
2020-06-19 13:46:10 +0200

    SE Doc for RouteNode file

--- phase 2/src/mazeAI/Scorer.java
+++ phase 2/src/mazeAI/Scorer.java
@@ -1,5 +1,11 @@
 package mazeAI;
 
 public interface Scorer<T extends GraphNode> {
+    /**
+     *
+     * @param from
+     * @param to
+     * @return
+     */
     double computeCost(T from, T to);
 }
